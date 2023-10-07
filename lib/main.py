f081ceb8962b52dcbeb83b4ebf90c3094ad6daf5
qiuzilay <qiuzilay0410@gmail.com>
2023-10-07 17:49:31 +0800

    delete .gitignore

--- lib/main.py
+++ lib/main.py
@@ -1,9 +1,36 @@
-import tellopy as Tellopy
+from Tello3 import send, sock
+from time import sleep
+from typing import overload
+# import tellopy as Tellopy
 
-"""class drone: ...
+class drone:
+
+    @staticmethod
+    def takeoff(dist):
+        send(f'takeoff {dist}')
+
+
+    @staticmethod
+    def land(dist):
+        send(f'land {dist}')
+
+
+
+drone.takeoff(30)
+sleep(3)
+drone.land(30)
+
+sock.close()
+
+"""
 drone:Tellopy.Tello = Tellopy.Tello()
 
 drone.connect()
-drone.wait_for_connection()"""
-cmd = input()
-print(cmd.encode())
+drone.wait_for_connection()
+
+drone.takeoff()
+sleep(3)
+drone.forward(30)
+drone.backward(30)
+drone.land()
+"""
