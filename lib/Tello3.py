f1904af1fc19c7c5605cb3152495b999f0326160
qiuzilay <qiuzilay0410@gmail.com>
2023-10-07 17:12:34 +0800

    小修改，之後打算用從文字檔讀取指令的方式去輸入指令

--- lib/Tello3.py
+++ lib/Tello3.py
@@ -10,27 +10,30 @@ port = 9000
 locaddr = (host, port) 
 
 
-# Create a UDP socket
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 tello_address = ('192.168.10.1', 8889)
 
 sock.bind(locaddr)
 
+
+
 def recv():
     count = 0
-    while True: 
+    while True:
         try:
             data, server = sock.recvfrom(1518)
-            print(data.decode(encoding="utf-8"))
+            data.decode(encoding="utf-8")
+            #print(data.decode(encoding="utf-8"))
         except Exception:
-            print ('\nExit . . .\n')
+            console.info("Exit from recv() since an unknown exception was triggered.")
             break
 
 def send(cmd:str):
     try:
         sock.sendto(cmd.encode(encoding="utf-8"), tello_address)
-        # console.log(f"Execute commands: {cmd}, set exec time as {delay} second{'s' if delay > 1 else ''}")
+        #console.log(f"Execute commands: {cmd}, set exec time as {delay} second{'s' if delay > 1 else ''}")
     except Exception as _E:
         console.warn(_E)
 
@@ -52,7 +55,7 @@ print ('Tello: command takeoff land flip forward back left right \r\n       up d
 print ('end -- quit demo.\r\n')"""
 
 
-#recvThread create
+
 recvThread = threading.Thread(target=recv)
 recvThread.start()
 
@@ -61,20 +64,17 @@ while True:
     try:
         cmd = input("<Tello> ")
 
-        if not cmd:
-            break  
-
-        elif 'end' in cmd:
-            print ("...")
-            sock.close()  
+        if not cmd or 'end' in cmd:
+            sock.close()
+            console.info("socket was closed.")
             break
+
         else:
-            send(*Gadget.multinput(cmd))
+            send(cmd)
+
     except KeyboardInterrupt:
-        print ("\n . . .\n")
-        sock.close()  
+        sock.close()
+        console.info("socket was closed cause the exception 'KeyboardInterrupt' was triggered.")
         break
 
-
-
-
+console.info("Ending.")
