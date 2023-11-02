735d365a6dc1bb313695983113f548b31090465c
qiuzilay <qiuzilay0410@gmail.com>
2023-11-02 17:08:26 +0800

    v1.0.0 with comments

--- lib/Tello3.py
+++ lib/Tello3.py
@@ -1,55 +1,63 @@
-import threading 
+import threading
 import socket
-import sys
 from time import sleep
 import cv2
-from toolbox import Gadget, console
+from toolbox import console
 from os import chdir, path
 from main import Main
 
-chdir(path.dirname(path.realpath(__file__)))
+chdir(path.dirname(path.realpath(__file__))) # 設定終端執行位置為此程式所在之資料夾
 
-class Tello: ...
-class const:
-    class addr:
-        host = ('0.0.0.0', 9000)
-        tello = ('192.168.10.1', 8889)
-        stream = 'udp://@0.0.0.0:11111'
+class Tello: ... # 只是變色用
+class const: # 常數 (參數) 集。只是為了讓變數可以像 javascript 的 json 的存取寫法才刻意寫成這樣
+    class addr_prototype: # 位址集。假如要存取 host，可以直接用 const.addr_prototype.host 讀取
+        host = ('0.0.0.0', 9000) # 電腦與無人機連線的 (UDP/IP, 埠號)
+        tello = ('192.168.10.1', 8889) # 無人機與電腦連線的 (UDP/IP, 埠號)
+        stream = 'udp://@0.0.0.0:11111' # 無人機的影像傳輸位址
+    addr = addr_prototype # 讓變數集變回普通變數的顏色 (淺藍色)
+    # 把變數集的 className 加個 prototype 在後面是因為如果直接用 addr，
+    # "const.addr.host" 中會有兩個變數持有類別的顏色 (const and addr)
+    # 但這樣就會跟 javascript 的 json 外貌不一樣
+    # 所以 class 的名字才特意宣告成不同的樣子，之後再讓一般變數顏色的 addr 去指向它
+    # 沒戳，只是為了美觀 (X
+
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    stream = cv2.VideoCapture(addr.stream)
+    stream = cv2.VideoCapture(addr.stream) # 從給定的位址，捕捉影像。因為同樣在 const 內，所以這邊存取 addr 不用打 const
     sock.bind(addr.host)
 
 
-T = 3
+T = 3 # 如果 VideoCapture 捕捉失敗，重新嘗試連線。(雖然如果第一次失敗後面再試幾次極大概率也不會成功啦 030)
 for _ in range(T) if not const.stream.isOpened() else ():
     console.info(f'cv2.VideoCapture failed to initialize. {T-_} retrying {"chance" if _ <= 1 else "chances"} left.')
     if const.stream.open(const.addr.stream):
         break
     else: sleep(1) if (_ + 1 < T) else console.info(f'Failed to initial camera!')
 
-
+# 把參數集送進 main.py 裡面的 Main 類別裡面，創建實例，命名Tello。Main 的所有內容請至 main.py 查看。
 Tello:Main = Main(const)
 
-
+# 創建新執行緒 (多工/並行處理)，執行 Main 下方的 recv() 方法
 recvThread = threading.Thread(target=Tello.recv)
-recvThread.daemon = True
-recvThread.start()
+recvThread.daemon = True # 如果主程式 (主執行緒) 中斷，就同步中止 recvThread 這個執行緒
+recvThread.start() # 開始執行
 
-Tello.queue.append(('command', None, 0.1))
+# 將 command 和 streamon 兩個指令添加進指令隊列，待會供 Main.exec() 執行，為傳給無人機讓其在最一開始時執行。
+# 兩個指令作用請參見 sources 資料夾內的 "Tello SDK 2.0 User Guide.pdf" 內的說明
+Tello.queue.append(('command', None, 0.1)) # （指令, 指令參數, 延遲秒數）
 Tello.queue.append(('streamon', None, 0.1))
 
-if const.stream.isOpened():
+if const.stream.isOpened(): # 如果 cv2.VideoCapture 有成功啟動，才建立接收影像用的執行緒，否則跳過。
     recvideoThread = threading.Thread(target=Tello.recvideo)
     recvideoThread.daemon = True
     recvideoThread.start()
 
-Tello.load()
+Tello.load() # 即 Main.load()，載入 "commands.txt" 內設定的指令
 
-execThread = threading.Thread(target=Tello.execute)
+execThread = threading.Thread(target=Tello.execute) # Main.execute() 的執行緒，
 execThread.daemon = True
 execThread.start()
 
-Tello.console()
+Tello.console() # 由主程式處理(監聽)自終端機輸入的指令
 
-console.info('Ending')
+console.info('Ending') # 主程序終止
--- lib/main.py
+++ lib/main.py
@@ -1,40 +1,48 @@
-from typing import Iterable, Sequence, Callable
+from typing import Callable
 from toolbox import Gadget, console
 from time import sleep
+from datetime import datetime
 import socket
 import cv2
 
 class Main:
 
     def __init__(self, const):
-        class __response_prototype:
+        class __response_prototype: # 儲存無人機回傳的訊息。(在 Main.recv() 那邊賦值)
             def __init__(self):
                 self.__msg = ''
+                self.ignore = False
 
+            # 用 self.response.get 可以在取值的同時清除儲存值
             @property
             def get(self):
                 final, self.__msg = self.__msg, ''
                 return final
             
+            # self.response.value 就只是一般的讀取/寫入
+            # (其實沒必要，只是為了讓格式跟 self.response.get 相同)
             @property
             def value(self):
                 return self.__msg
             
             @value.setter
             def value(self, value):
-                self.__msg = value
+                if not self.ignore: self.__msg = value
 
 
-        self.sock: socket.socket
-        self.stream: cv2.VideoCapture
-        self.power = True
-        self.queue = list()
-        self.sock = const.sock
-        self.stream = const.stream
-        self.addr = const.addr
-        self.response = __response_prototype()
+        self.sock: socket.socket # 註記 self.sock 等等會拿到 socket.socket 的類別物件 (寫給 vscode 看的)
+        self.stream: cv2.VideoCapture # 註記 self.stream 等等會拿到 cv2.VideoCapture 的物件
+        self.power = True # 控制 while 迴圈的開關 (之前因為不知道threading有daemon可以控制同步關閉才加了這東西)
+        self.queue = list() # 指令佇列。Main.load() 讀取的指令都會塞進這裡等待 Main.exec() 處理
+        self.sock = const.sock # 在這邊才正式給 self.sock 賦值
+        self.stream = const.stream # 拿 cv2.VideoCapture 物件
+        self.addr = const.addr # 拿位址集 (class addr)
+        self.response = __response_prototype() # 因為 @property 只能用在物件上，所以這邊把 __response_prototype 實例化
 
-    def _execore(self, cmdl:tuple, caller:Callable):
+    # 執行核心
+    # (因為 Main.console() 跟 Main.execute() 這部分的功能重疊)
+    # (所以把這部分從原先的 Main.execute() 中獨立出來)
+    def _execore(self, cmdl:tuple, caller:Callable, ignore:bool=False):
         ###---- valid cmdl -----###
         # (command)               #
         # (command, value)        #
@@ -55,36 +63,55 @@ class Main:
                 args.command, args.value, args.delay = cmdl
             case _:
                 raise ValueError('illegal number of arguments occurred at Main.execute()')
-            
+        
+        # print 出準備送出的指令資訊
         console.info(
             f"\u2714 {'Loaded' if (caller.__name__.__ne__('console')) else 'Console'} commands: {args.command}",
             f"- value: {args.value if (args.value) else 'unspecified'}",
             f"- delay: {args.delay if (args.delay) else 'unspecified'}",
             f"- from: {caller.__name__}", sep='\n', end='\n\n'
         )
-        self.send(args.command if (not args.value) else f'{args.command} {args.value}')
+        self.response.ignore = ignore # 是否要 self.response 忽略記錄無人機回傳的消息
+        self.send(args.command if (not args.value) else f'{args.command} {args.value}') # 發送指令至無人機
 
         return args
 
+    # 執行器 (負責處理佇列中的指令)
     def execute(self):
-        # console.load(console.info, 'Function.exec() is executing now')
         while self.power:
+            # 如果沒指令佇列，等待一秒後再次進入迴圈
             if not self.queue:
                 sleep(1)
                 continue
-            self.queue[0]
+            
+            # 如果執行到這邊代表有指令存在於佇列中，開始處理
+
+            # 把指令送進執行核心內，表明呼叫者是 Main.execute()
+            # 最後再捕捉 Main._execore() 回傳的指令參數 (class args)
             args = self._execore(
                 cmdl = self.queue[0],
                 caller = self.execute
             )
-            console.info('response:', self.response.value)
-            if not self.response.get.lower().__eq__('error not joystick'):
-                self.queue.pop(0)
-                sleep(args.delay) if args.delay else ...
-            else:
-                console.info(f'Congestion occurred. Would try to re-execute the command "{args.command}" in 0.5 seconds')
-                sleep(0.5)
 
+            # 這段是用來防止指令發生碰撞被吃導致後面的動作大亂而添加的保險
+            while True: # 重複檢測，直到取得來自無人機的回應(response)
+                if self.response.value:
+
+                    console.info('response:', self.response.value)
+
+                    # 'error not joystick' 是無人機接收指令時若發生衝突會回傳的訊息
+                    if not self.response.get.lower().__eq__('error not joystick'): # 如果無碰撞，進入 if 程式區塊
+                        self.queue.pop(0) # 把佇列中最前面 (當前) 的指令移除
+                        sleep(args.delay) if args.delay else ... # 如果該指令有設定延遲，處理延遲
+                    else:
+                        # 發生碰撞，發送訊息，且不將指令從佇列中移除
+                        console.load(console.info, f'Congestion occurred. Would try to re-execute the command "{args.command}" in 3 seconds')
+
+                    break # 離開此迴圈，回到 Main.execute() 的開頭
+
+                else: sleep(0.1)
+
+    # 載入器 (負責讀取'commands.txt'內的指令清單，並存入佇列中)
     def load(self):
         with open('./commands.txt', mode='r', encoding='UTF-8') as _cmdfile:
             for line in _cmdfile:
@@ -98,6 +125,7 @@ class Main:
             ''.center(32, '-'), sep='\n'
         ) if self.queue else console.info(f'Command list was empty.')
     
+    # 終端機輸入監聽器
     def console(self):
         while self.power: 
 
@@ -139,62 +167,87 @@ class Main:
                             console.info(f'The command "{userInput[1:]}" was not found!', end='\n\n')
 
                 else:
-                    #self.queue.insert(0, Gadget.formatter(userInput, split=','))
+                    # 不是空白，也不是'end'，沒有'!'前墜，那就視為要傳給無人機的指令
+                    # 越過 Main.execute() 將指令強制餵給無人機，所以要 self.response 不要記錄回應
+                    # 這設計可能有小瑕疵，畢竟是指令的發送和回應的接收是異步
+                    # 如果 Main.execute() 跟 Main.console() 送出指令的時間太過接近大概會讓收到的回應內容錯亂
+                    # 之後有空再改
                     self._execore(
                         cmdl = Gadget.formatter(userInput, split=','),
-                        caller=self.console
+                        caller=self.console,
+                        ignore=True
                     )
 
+            # 強制關閉終端機會觸發此例外，終止程式
             except KeyboardInterrupt:
                 self.power = False
                 self.sock.close()
                 console.info('Socket was closed cause the exception "KeyboardInterrupt" was triggered')
                 break
-
+            
+            # 使用'!stop'會觸發此例外，終止程式
             except CommandOverrideException:
                 self.power = False
                 self.queue = list()
-                self.send('land')
-                console.info('Tello was forced to land cause an overriding command was triggered')
                 self.sock.close()
+                console.info('Tello was forced to land cause an overriding command was triggered')
                 break
 
-    #---------------- Custom Functions END ----------------#
-
+    # 將指令發送給無人機
     def send(self, cmdl:str):
         try:
             self.sock.sendto(cmdl.encode(encoding='UTF-8'), self.addr.tello) # @IgnoreException
         except Exception as _E:
             console.warn(_E)
 
+    # 讀取無人機回傳的影像資料
     def recvideo(self):
-        cv2.CAP_PROP_FRAME_WIDTH
-        while self.power:
+        while self.power: # 最外層迴圈 (因為是設計讓鏡頭可以開開關關)
             while self.stream.isOpened():
+                # 有影像串流再執行內層迴圈
+                # 無人機回傳的是 影像檔(image) 而不是 影片檔(video)
+                # 所以假設如果要 60fps 就等於要讓這迴圈 1 秒跑 60 次。
                 try:
+                    # 由 cv2.VideoCapture 解析它拿到影像
                     _, frame = self.stream.read() # @IgnoreException
+                    # cv2 創建視窗顯示影像。視窗名設為'Tello'
                     cv2.imshow('Tello', frame) # @IgnoreException
-                    #cv2.imshow("Tello", cv2.resize(frame, (
-                    #    int(self.stream.get(cv2.CAP_PROP_FRAME_WIDTH)),
-                    #    int(self.stream.get(cv2.CAP_PROP_FRAME_HEIGHT))
-                    #))) # @IgnoreException
-                    cv2.waitKey(16)
+
+                    ## ----------- ##
+                    # Enter -> 13   #
+                    # Space -> 32   #
+                    # Esc   -> 27   #
+                    ## ----------- ##
+
+                    # cv2.waitkey(16) 會讓視窗停佇 16 毫秒 (60fps = 1frame / 16.6ms)
+                    # 期間如果 Enter 鍵被按下則進入 if 區塊
+                    if (cv2.waitKey(16) & 0xFF).__eq__(13):
+                        T = datetime.now() # 取得需要用在影像檔的檔名上的時間
+                        path = (
+                            fr'../screenshots/'
+                            fr'{T.year}-{T.month}-{T.day} {T.hour}.{T.minute}.{T.second}.{str(T.microsecond)[0]}.jpg'
+                        )
+                        console.info(f'Save screenshots to', path)
+                        # 斷言，如果 cv2.imwrite() 回傳 False（即儲存失敗）則觸發 AssertionError
+                        assert cv2.imwrite(path, frame), "Failed to save the screeshots"
+                        
                 except Exception as _:
                     console.info(_)
-            self.stream.release()
-            cv2.destroyAllWindows()
 
+            self.stream.release() # 關閉鏡頭
+            cv2.destroyAllWindows() # 摧毀所有 cv2 創建的視窗
+
+    # 接收無人機回傳的訊息
     def recv(self):
         while self.power:
             try:
                 retval, _ = self.sock.recvfrom(1518) # @IgnoreException
                 self.response.value = retval.decode()
-                console.log('<Tello>', self.response.value, end='\n\n')
             except Exception as _:
                 console.info(_)
     
 
-class CommandOverrideException(Exception):
+class CommandOverrideException(Exception): # 只是一個自定義的例外類別
     
     def __init__(self):
         self.message = 'Tello was forced to land cause an overriding command was triggered.'
